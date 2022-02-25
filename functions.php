5e37b9d1e20f3772a3062cda32e7ba977c32c793
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-02-25 15:52:09 +0100

    preparation to moving block's styles to plugin

--- functions.php
+++ functions.php
@@ -1,11 +1,21 @@
 <?php
 
 function custom_enqueue(){
-    wp_enqueue_style('customstyle', get_stylesheet_uri(), array(), '1.0.0', 'all' );
-    wp_enqueue_style('customstyle', get_template_directory_uri() . '/dist/main.css', array(), '1.0.0', 'all' );
-    wp_enqueue_script('customjs', get_template_directory_uri() . '/dist/app.js', array(), '1.0.0', 'true' );
+    wp_enqueue_style('themestyle', get_stylesheet_uri(), array(), '1.0.0', 'all' );
+    wp_enqueue_style('mainstyles', get_template_directory_uri() . '/dist/main.css', array(), '1.0.0', 'all' );
+    wp_enqueue_script('mainjs', get_template_directory_uri() . '/dist/app.js', array(), '1.0.0', 'true' );
 }
 add_action('wp_enqueue_scripts', 'custom_enqueue');
+
+
+function wpdocs_selectively_enqueue_admin_script( $hook ) {
+    if ( 'post.php' != $hook ) {
+        return;
+    }
+    wp_enqueue_script( 'mainstyles-admin', get_template_directory_uri() . '/dist/main.css', array(), '1.0.0', false );
+}
+add_action( 'admin_enqueue_scripts', 'wpdocs_selectively_enqueue_admin_script' );
+
 
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
@@ -25,9 +35,28 @@ add_image_size( 'header-desktop-1x', 585, 584);
 
 function register_my_menus() {
     register_nav_menus(
-      array(
-        'header-menu' => __( 'Header Menu' )
-      )
+        array(
+            'header-menu' => __( 'Header Menu' )
+        )
     );
-  }
-  add_action( 'init', 'register_my_menus' );
+}
+add_action( 'init', 'register_my_menus' );
+
+// add_action('acf/init', 'my_acf_init_block_types');
+// function my_acf_init_block_types() {
+
+//     // Check function exists.
+//     if( function_exists('acf_register_block_type') ) {
+
+//         // register a testimonial block.
+//         acf_register_block_type(array(
+//             'name'              => 'logos-list',
+//             'title'             => __('Logos list'),
+//             'description'       => __('Block with list of logos'),
+//             'render_template'   => 'blocks/logos-list/logos-list.php',
+//             'category'          => 'formatting',
+//             'icon'              => 'admin-comments',
+//             'keywords'          => array( 'testimonial', 'quote' ),
+//         ));
+//     }
+// }
--- header.php
+++ header.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php the_title(); ?></title>
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/dist/main.css' ?>">
   <?php
     wp_head();
 ?>
