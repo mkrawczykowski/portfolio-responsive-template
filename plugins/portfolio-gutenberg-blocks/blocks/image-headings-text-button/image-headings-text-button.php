19f9fe2353c3c218d75bfcf6566605fe077fc730
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-02-28 16:55:04 +0100

    added ACF fields to second block

--- plugins/portfolio-gutenberg-blocks/blocks/image-headings-text-button/image-headings-text-button.php
+++ plugins/portfolio-gutenberg-blocks/blocks/image-headings-text-button/image-headings-text-button.php
@@ -31,11 +31,11 @@ if( !empty($block['className']) ) {
             <div class="col-12 col-md-6">
 
             <div class="headings-text headings-text--dark">
-                <h3 class="headings-text__small-above"><?php the_field('small-heading', 'options'); ?></h3>
-                <h1 class="headings-text__big headings-text__big--size-medium"><?php the_field('big-heading', 'options'); ?></h1>
-                <h3 class="headings-text__small-below"><?php the_field('small-heading', 'options'); ?></h3>
-                <p class="headings-text__paragraph"><?php the_field('paragraph', 'options'); ?></p>
-                <a href="<?php the_field('button-1__url', 'options'); ?>" class="button button--white-with-shadow button--width-narrow"><?php the_field('button-1__text', 'options'); ?></a>
+                <h3 class="headings-text__small-above"><?php the_field('small-heading-above'); ?></h3>
+                <h1 class="headings-text__big headings-text__big--size-medium"><?php the_field('big-heading'); ?></h1>
+                <h3 class="headings-text__small-below"><?php the_field('small-heading-below'); ?></h3>
+                <p class="headings-text__paragraph"><?php the_field('paragraph'); ?></p>
+                <a href="<?php the_field('button__link'); ?>" class="button button--white-with-shadow button--width-narrow"><?php the_field('button__caption'); ?></a>
             </div>
 
             </div>
